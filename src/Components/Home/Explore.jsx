9494a9be1671690cd547e6c7ccb9f0ea954aaf6c
afrye1997 <allisonfrye97@gmail.com>
2020-07-09 20:09:43 -0500

    getting uesrs

--- src/Components/Home/Explore.jsx
+++ src/Components/Home/Explore.jsx
@@ -4,8 +4,30 @@ import Spacer from 'terra-spacer';
 import ActionHeader from 'terra-action-header'
 import Checkbox from 'terra-form-checkbox';
 import Profile from '../Profile/Profile';
-
+import {useState, useEffect} from 'react'
 function Explore(){
+
+    //const [ppl, setPpl]=useState
+    //fetch function
+    const [result, setResult]=useState([])
+    useEffect(() => {
+        fetch("https://internmingle-be.herokuapp.com/user/getUser")
+          .then(res => res.json())
+          .then(
+            (response) => {
+              setResult(response);
+           
+            },
+            // Note: it's important to handle errors here
+            // instead of a catch() block so that we don't swallow
+            // exceptions from actual bugs in components.
+            (error) => {
+            
+            }
+          )
+      }, [])
+
+      console.log(result)
     return(
         <Spacer paddingTop="medium" paddingLeft="medium" paddingRight="medium">
             <Card variant="raised" style={{width: "1500px", marginLeft: "auto", marginRight: "auto"}}>
@@ -17,6 +39,9 @@ function Explore(){
                         <Checkbox id="third" labelText="Third Checkbox" />
                     </div>
                     <div style={{float:'left', paddingLeft: '20px', width: "1200px"}}>
+                        {/* {response.map(x=>{
+                            <
+                        })} */}
                         <Profile />
                         <Profile />
                         <Profile />
